96a74f17f7270f6bd921479dfbfc80f5d8cb179d
SFarida <faridagueleu@gmail.com>
2023-03-31 16:29:20 +0100

    Update weight_kg of animals

--- queries.sql
+++ queries.sql
@@ -24,3 +24,12 @@ COMMIT TRANSACTION;
 BEGIN TRANSACTION;
 DELETE FROM animals;
 ROLLBACK TRANSACTION;
+
+/* Update weight_kg */
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT first_savepoint;
+UPDATE animals SET weight_kg = weight_kg * - 1;
+ROLLBACK TO SAVEPOINT first_savepoint;
+UPDATE animals SET weight_kg = weight_kg * - 1 WHERE weight_kg < 0;
+COMMIT TRANSACTION;
