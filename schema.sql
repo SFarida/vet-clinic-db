f205467ee3ccaa1fd0beb4d22c611fcf4f323c6a
SFarida <faridagueleu@gmail.com>
2023-03-31 21:17:42 +0100

    Add foreign keys to animals table

--- schema.sql
+++ schema.sql
@@ -25,3 +25,12 @@ CREATE TABLE IF NOT EXISTS species (
    id serial PRIMARY KEY,
    name VARCHAR ( 100 ) UNIQUE NOT NULL
 );
+
+/* Remove species from animals table */
+ALTER TABLE animals DROP COLUMN species;
+
+/* Add foreign keys */
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) MATCH FULL;
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id) MATCH FULL;
